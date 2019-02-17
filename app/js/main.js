478142a55e85a4503baa0f2c3039171ce6043682
Evgeniy Sheleshkov <e.sheleshkov@gmail.com>
2019-02-17 15:11:20 +0300

    Исправления после созвона

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,12 @@
 let menu = (function(options) {
     let buttonClick = document.querySelector(options.buttonClick);
     let menu = document.querySelector(options.menu);
+    let body = document.querySelector('body');
 
-    let _toggleMenu = function(e) {
+    let _toggleMenu = function() {
         buttonClick.classList.toggle("is-active");
         menu.classList.toggle("is-active");
+        body.classList.toggle('is-locked');
         console.log(buttonClick);
         console.log(menu);
     }
@@ -12,11 +14,31 @@ let menu = (function(options) {
         buttonClick.addEventListener("click", _toggleMenu); 
     }
 
-    return {open: addListeners};
+    return {
+            open: addListeners
+            };
 }) ({
     buttonClick: "#hamburger-menu-link",
     menu: "#hamburger-menu"
 });
 
 
+//menu.open();
 menu.open();
+
+slider = (function(){   
+
+    let moveslider = function(){
+
+    }
+
+    let addListeners= function () {
+        moveslider();
+    }
+
+    return {
+        init:addListeners
+    }
+})();
+
+slider.init();
