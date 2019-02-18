73a8e5452508adf18b831a431c5ba78e83604fa6
Evgeniy Sheleshkov <e.sheleshkov@gmail.com>
2019-02-18 18:37:32 +0300

    Аккардионы

--- app/js/main.js
+++ app/js/main.js
@@ -42,3 +42,66 @@ slider = (function(){
 })();
 
 slider.init();
+
+let teamAccoJS = () => {
+    let oTeamLink = document.querySelectorAll(".team-accordion__link");
+    oTeamLink.forEach(function(personName) {
+        personName.addEventListener("click", function(e) {
+            e.preventDefault();
+            let activePerson = document.querySelector(".team-accordion__item.is-active");
+
+            if (activePerson) {
+                let teamAccordionDetails = activePerson.querySelector(".team-accordion__details")
+
+                teamAccordionDetails.style.height = "0px";
+                activePerson.classList.remove("is-active");
+            }
+            
+            if(!activePerson || activePerson.querySelector(".team-accordion__link") !== e.target){
+                let currentPerson = e.target.closest(".team-accordion__item");
+                currentPerson.classList.add("is-active");
+
+                let currentPersonInfo = currentPerson.querySelector(".team-accordion__details");
+                currentPersonInfo.style.height = currentPersonInfo.scrollHeight + "px";
+            }
+        })
+    })
+};
+teamAccoJS();
+
+
+let verticalAcco = () => {
+
+    let calculateWidth = () => {
+        let windowWidth = window.innerWidth;
+        
+        let links = document.querySelectorAll(".menu-acco__trigger");
+        let linksWidht = parseFloat(getComputedStyle(links[0]).width);
+        let reqWidth = windowWidth - linksWidht * links.clientHeight;
+        return reqWidth > 550 ? 550 :reqWidth;
+    };
+
+    let oTeamLink = document.querySelectorAll(".menu-acco__trigger");
+    oTeamLink.forEach(function(personName) {
+        personName.addEventListener("click", function(e) {
+            e.preventDefault();
+            let activePerson = document.querySelector(".menu-acco__item.active");
+
+            if (activePerson) {
+                let teamAccordionDetails = activePerson.querySelector(".menu-acco__content")
+
+                teamAccordionDetails.style.width = "0px";
+                activePerson.classList.remove("active");
+            }
+            
+            if(!activePerson || activePerson.querySelector(".menu-acco__trigger") !== e.target){
+                let currentPerson = e.target.closest(".menu-acco__item");
+                currentPerson.classList.add("active");
+
+                let currentPersonInfo = currentPerson.querySelector(".menu-acco__content");
+                currentPersonInfo.style.width = calculateWidth() + "px";
+            }
+        })
+    })
+};
+verticalAcco();
